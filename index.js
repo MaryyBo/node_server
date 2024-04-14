f9450b625b03d90e941df8b1c75bc40d84e8eb24
MaryyBo <marusya.bodrug@gmail.com>
2024-04-14 14:28:27 +0200

    Server side rendering example

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const http = require('http')
+const fs = require('fs/promises')
 
 const PORT = 5000;
 
 const requestListener = (request, response) => {
-    const { url, method } = request
-    console.log(url, method);
+    const { url } = request
+    
+    if (url === '/index.html') { // http://localhost:5000/index.html
+        fs.readFile('./views/index.html', 'utf-8')
+        .then (data => {
+            response.statusCode = 200;
+            response.end(data);
+        })
+    } else {
+        response.statusCode = 404;
+        response.end()
+    }
 
-    response.statusCode = 404;
-    response.end('Hello from server')
-
-    // console.log(response);
 }
 
 const server = http.createServer(requestListener);
