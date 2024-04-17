582f778a456f3e1f446d66d594b98d80b4615711
MaryyBo <marusya.bodrug@gmail.com>
2024-04-17 18:08:39 +0200

    yup validation

--- index.js
+++ index.js
@@ -1,22 +1,46 @@
 // Шлях + метод - це роут 
 
 const express = require('express');
+const yup = require('yup')
+
 const app = express(); // додаток відловили в змінній
 
+const bodyParser = express.json(); // request.body
+
+const validationSchema = yup.object({
+    firstName: yup.string().required(),
+    lastName: yup.string().required(),
+    email: yup.string().email().required(),
+    password: yup.string().required(),
+    isSubscribed: yup.boolean().required()
+
+})
 const PORT = 5000;
 
-app.get('/', (request, response) => { // http://localhost:5000/
-    response.send('Hello world');
-}, () => {
-    console.log('first request handler');
-})
+app.post('/user', bodyParser, async (request, response, next) => {
+    try {
+        console.log(request.body);
+        const value = await validationSchema.validate(request.body);
+        console.log(value);
 
-app.get('/index.html', (request, response) => {
-response.status(404).send('Test /index.html');
+    } catch (error) {
+        response.status(400).send(error.message);
+    }
 })
-
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 })
 
+
+
+/*
+Задача: зареєструвати юзера
+
+1. Прийняти запит на певний роут
+2. Розпарсити дані, які прийшли з запитом
+3. Валідувати дані
+4. Зберегти ці дані
+5. Надіслати відповідь клієнту
+
+*/
